09783375c1c682d3a0095b81a17d8c43be7fcec3
João Victor <69174195+galvao845@users.noreply.github.com>
2023-07-10 22:27:59 -0300

    Update cssImports.js

--- src/frontend/css/cssImports.js
+++ src/frontend/css/cssImports.js
@@ -3,6 +3,5 @@ import './cssFiles/index.css';
 import './cssFiles/login.css';
 import './cssFiles/sidebar.css';
 import './cssFiles/logoutPage.css';
-import './cssFiles/covid19.css'
 
 export const importCSS = () => {};
